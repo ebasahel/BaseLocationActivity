85e13e727a4505b19f855860f3d393010a391c4b
emanbasahel <Eman0500821725>
2020-03-16 19:13:29 +0300

    update the project with the latest Location API

---
+++ app/src/main/java/net/techiebits/locationutilsapp/MainActivity.java
@@ -0,0 +1,68 @@
+package net.techiebits.locationutilsapp;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends net.techiebits.locationutilsapp.BaseLocationActivity
+        implements OnMapReadyCallback, net.techiebits.locationutilsapp.BaseLocationActivity.onLocationConnected {
+
+    private GoogleMap mMap;
+    private MarkerOptions mMarkerOptions;
+    private Marker mMarker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //displaying the map
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        /**
+         * to display the blue marker that indicated user's current location
+         * (it needs user's permission)
+         */
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED)
+            mMap.setMyLocationEnabled(true);
+        else checkPermission();
+
+    }
+
+    //region to add marker on map
+    public void addMarker(final LatLng latlng, final int zoom) {
+        mMarkerOptions = new MarkerOptions().position(latlng);
+        mMarker= mMap.addMarker(mMarkerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
+    }
+    //endregion
+
+    /**
+     *
+     * @param location Firstly, it's the last known location then it's received from the locationUpdates
+     */
+    @Override
+    public void getCurrentLocation(Location location) {
+        /**
+         * if the marker is NULL add marker else update its position to the latest received location
+         */
+        if (mMarker == null) {
+            addMarker(new LatLng(location.getLatitude(), location.getLongitude()), 15);
+        }else
+            mMarker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+
+    }
+}
